ef53f9b2270fedf7f936e38472ec17c4802c5ca7
Phat <dinhphat611@gmail.com>
2022-11-13 19:10:18 +0700

    update login

--- course-management/src/components/login-form.js
+++ course-management/src/components/login-form.js
@@ -9,8 +9,15 @@ const LoginForm = () => {
             <img src={loginImg} width="300" alt='login-page' />
             <div className="container">
                 <h1>LOGIN</h1>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
+                <div className='input-container'>
+                    <i class="fa fa-user icon"></i>
+                    <input type="text" placeholder="Username" />
+                </div>
+
+                <div className='input-container'>
+                    <i class="fa fa-key icon"></i>
+                    <input type="password" placeholder="Password" />
+                </div>
                 <div className="login-btn" >LOGIN</div>
             </div>
         </div>
