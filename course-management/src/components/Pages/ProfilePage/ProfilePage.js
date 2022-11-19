b1ac3fdbc97993e4ceb5218084a702d0aadee074
pthienquan1 <quan01011997123@gmail.com>
2022-11-19 13:06:55 +0700

    Edit profile page

--- course-management/src/components/Pages/ProfilePage/ProfilePage.js
+++ course-management/src/components/Pages/ProfilePage/ProfilePage.js
@@ -5,15 +5,15 @@ import './ProfilePage.css'
 
 
 
-function ProfilePage () {
-    return(
+function ProfilePage() {
+    return (
         <div className="profile">
-            <Navbar title="ACCOUNT"/>
+            <Navbar title="ACCOUNT" />
             <img src={avatar} className='profile-avatar' />
             <div className="profile-header">
                 <h4>Profile</h4>
                 <div className='profile-content'>
-                    <h2>Phạm Tiến Trung</h2>
+                    <h2>Phạm Tiên Trung</h2>
                     <p>10-4-1994</p>
                     <p>phamtientrung94@traindemy.edu.vn</p>
                 </div>
@@ -22,7 +22,7 @@ function ProfilePage () {
                     <p>Information system trainer</p>
                 </div>
             </div>
-            
+
         </div>
     )
 }
