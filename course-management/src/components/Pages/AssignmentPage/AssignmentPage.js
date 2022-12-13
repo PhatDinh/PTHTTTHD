49611d605f6a8e484e09ff31bf1897b7a7e577ce
Phat <dinhphat611@gmail.com>
2022-12-13 18:49:54 +0700

    add btn add assignment

--- course-management/src/components/Pages/AssignmentPage/AssignmentPage.js
+++ course-management/src/components/Pages/AssignmentPage/AssignmentPage.js
@@ -1,12 +1,16 @@
 import AssignmentCard from "../../AssignmentCard/AssignmentCard";
 import Navbar from "../../Navbar/Navbar"
 import './AssignmentPage.css';
+import Fab from '@mui/material/Fab';
+import { Button } from "@mui/material";
+
 
 
 const AssignmentsPage = () =>{
     return (
         <div className="assignmentpage">
             <Navbar title={'Centertric'}></Navbar>
+            <div className="assignmentpage-create"><p>Create</p></div>
             <AssignmentCard/>
             <AssignmentCard/>
             <AssignmentCard/>
